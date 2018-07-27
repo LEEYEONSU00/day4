9ed3002d46764d8ac091029bfa9b288684eeee1d
LEEYEONSU00 <abcd1520@naver.com>
2018-07-27 09:40:05 +0900

    class까지

--- function.py
+++ function.py
@@ -9,4 +9,4 @@ age = input("how old are you? ")
 hello(name, age )
 hello(name) # 매개변수 2번째값에 초기값 설정해주면 오류나지 않음. ex) age= 10
 hello()
-hello(age = '30', name = 'Jay')
+hello(age = '30', name = 'Jay') # 매개변수 순서 바뀌어도 상관 없음.
